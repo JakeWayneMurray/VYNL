f6182d3bb9b255281abc6338093b267e19ace02f
Jake Murray <109402063+JakeWayneMurray@users.noreply.github.com>
2022-08-01 21:10:56 -0400

    Update Account.cs

--- VinylCollectionApplication/Account.cs
+++ VinylCollectionApplication/Account.cs
@@ -51,7 +51,7 @@ namespace VinylCollectionApplication
         private static IMongoCollection<BsonDocument> getCollection()
         {
            
-            ```MongoClient dbClient = new MongoClient("mongodb+srv://DadsThrobber:BrianSucks@vynl.4q0um.mongodb.net/VynLDatabase?retryWrites=true&w=majority");
+            ```MongoClient dbClient = new MongoClient("mongodb+srv://DadsThrobber:BrianSucks@vynl.4q0um.mongodb.net/VynLDatabase?retryWrites=true&w=majority");```
             
             
             var database = dbClient.GetDatabase("VynLDatabase");
